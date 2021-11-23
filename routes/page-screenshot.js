1933d87560bf866cdafe387ac199e1ea887b8b33
Sean Zeliq Urian <seanurian@gmail.com>
2021-11-24 05:33:15 +0700

    Fixing deployment issue

--- routes/page-screenshot.js
+++ routes/page-screenshot.js
@@ -18,7 +18,9 @@ router.get("/", async (req, res, next) => {
   let height = parseFloat(params.height) || defaultHeight;
   let returnName = makeid(50);
 
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({
+    args: ["--no-sandbox", "--disable-setuid-sandbox"],
+  });
   const page = await browser.newPage();
   await page.goto(url, { waitUntil: "networkidle0" });
   await page.setViewport({ width: width, height: height });
